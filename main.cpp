0a88dd88e1816cc056698b48a4f96748afe833c2
Polarrobbe <robert-eller@gmx.de>
2017-01-18 17:49:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-
 #include <iostream>
 #include <conio.h>
 #include<Windows.h>
 using namespace std;
 
-bool gameOver;
+bool gameOver, movement;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
@@ -103,6 +102,8 @@ void Input()
 
 void Logic()
 {
+	int locationheadx = x;
+	int locationheady = y;
 	int prevX = tailX[0];
 	int prevY = tailY[0];
 	int prev2X, prev2Y;
@@ -119,44 +120,57 @@ void Logic()
 
 
 	}
+	movement = false;
 	switch (dir)
 	{
 	case LEFT :
-		if(prevX != x - 1)
+		//if (tailX[1] == x - 1)
+			//break;
 		x--;
+		movement = true;
 		break;
 	case RIGHT :
-		if (prevX != x + 1)
+		//if (tailX[1] == x + 1)
+			//break;
 		x++;
+		movement = true;
 		break;
 	case DOWN :
-		if (prevY != y + 1)
+		//if (tailY[1] == y + 1)
+			//break;
 		y++;
+		movement = true;
 		break;
 	case UP :
-		if (prevY != y - 1)
+		//if (tailY[1] == y - 1)
+			//break;
 		y-- ;
+		movement = true;
 		break;
 	default : 
 		break;
 	}
 	//if (x > width || x < 0 || y < 0 || y > height)
 		//gameOver = true;
-	if (x >= width-1) x = 1; 
-	if (x <= 0) x = width - 2;
-	if (y >= height) y = 0;
-	if (y < 0) y = height - 1;
-	for (int i = 0; i < nTail; i++)
-		if (tailX[i] == x && tailY[i] == y)
-			gameOver = true;
-	if (x == fruitX && y == fruitY)
+	if (movement == true)
 	{
-		score += 10;
-		nTail++;
-		fruitX = rand() % width;
-		fruitY = rand() % height;
-		
+		if (x >= width - 1) x = 1;
+		if (x <= 0) x = width - 2;
+		if (y >= height) y = 0;
+		if (y < 0) y = height - 1;
+		for (int i = 0; i < nTail; i++)
+			if (tailX[i] == x && tailY[i] == y)
+				gameOver = true;
+		if (x == fruitX && y == fruitY)
+		{
+			score += 10;
+			nTail++;
+			fruitX = rand() % width;
+			fruitY = rand() % height;
+
+		}
 	}
+	
 }
 
 int main()
@@ -174,3 +188,4 @@ int main()
 	}
 		return 0;
 }
+
